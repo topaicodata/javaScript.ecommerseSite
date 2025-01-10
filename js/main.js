9feec6493c67f8565ae80db5f8de3baba1770b80
Biswash Rimal <topaicodata@gmail.com>
2025-01-10 23:50:54 +0545

    fetch the latest link

--- js/main.js
+++ js/main.js
@@ -17,6 +17,37 @@ document.querySelector('#cart-btn').onclick = () => {
 
 let searchForm = document.querySelector('.search-form');
 
+//Protocol
+// let protocol = window.location.protocol;
+// let domain = window.location.hostname;
+// console.log(`Protocol: ${protocol}`);
+// console.log(`Domin: ${domain}`);
+
+//Access variable - URL
+// console.log(process.env.TZ);
+
+async () => {
+    //Try catch
+    try {
+        await fetch('');
+    }
+}
+
+//Search bar keyword track
+let searchBar = document.getElementById('search-box');
+let productSearch = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    let searchStr = e.target.value;
+    let filterProducts = productSearch.filter((product) => {
+        return (
+            product.name.includes(searchStr) || 
+            product.house.includes(searchStr)
+        );
+    });
+    console.log(filterProducts);
+});
+
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
